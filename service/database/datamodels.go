d32fd62e0e52378a3595b8821444c96b6b90a489
Flavia Bonanni <bonanni.1187711@studenti.uniroma1.it>
2025-03-29 15:21:58 +0100

    Delete datamodels.go

--- service/database/datamodels.go
+++
@@ -1,7 +0,0 @@
-package datamodels
-
-func (c *Conversation) ConvFromDatabase(conv database.Conversation) {
-	c.ConversationID = conv.ConversationID
-	c.Participants = conv.Participants
-	c.LastMessage = conv.LastMessage
-}
