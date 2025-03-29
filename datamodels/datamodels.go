3e4275a88010cda1114229b9085a6b94469e4f98
Flavia Bonanni <bonanni.1187711@studenti.uniroma1.it>
2025-03-29 15:24:19 +0100

    Update datamodels.go

--- datamodels/datamodels.go
+++ datamodels/datamodels.go
@@ -1,4 +1,10 @@
-package database
+package datamodels
+
+type Conversation struct {
+	ConversationID string   `json:"conversation_id"`
+	Participants   []string `json:"participants"`
+	LastMessage    string   `json:"last_message,omitempty"` // omitempty allows the field to be optional
+}
 
 func (c *Conversation) ConvFromDatabase(conv database.Conversation) {
 	c.ConversationID = conv.ConversationID
