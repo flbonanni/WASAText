d84a28fd6ed64d7cf6d1568030d7e9942937f65c
Flavia Bonanni <bonanni.1187711@studenti.uniroma1.it>
2025-05-07 17:58:59 +0200

    more fixes for setusername func

--- service/api/user-actions.go
+++ service/api/user-actions.go
@@ -34,7 +34,7 @@ func (rt *_router) doLogin(w http.ResponseWriter, r *http.Request, ps httprouter
 func (rt *_router) getUserProfile(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
 	// Estrarre il token dall'header Authorization
 	token := getToken(r.Header.Get("Authorization"))
-	log.Printf("DEBUG: il token è = %d", token)
+	// log.Printf("DEBUG: il token è = %d", token)
 
 	// Popola l'utente richiedente con il token e verifica in DB
 	var requestUser User
