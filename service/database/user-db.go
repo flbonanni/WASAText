1cc981da9cf2d6f1e65ef455267859d3d8900c2e
Flavia Bonanni <bonanni.1187711@studenti.uniroma1.it>
2025-05-07 17:54:47 +0200

    fixed setusername func

--- service/database/user-db.go
+++ service/database/user-db.go
@@ -34,7 +34,7 @@ func (db *appdbimpl) CreateUser(u User) (User, error) {
 }
 
 func (db *appdbimpl) SetUsername(u User, username string) (User, error) {
-	res, err := db.c.Exec(`UPDATE users SET Username=? WHERE Id=? AND Username=?`, u.CurrentUsername, u.ID, username)
+	res, err := db.c.Exec(`UPDATE users SET Username=? WHERE Id=? AND Username=?`, username, u.ID, u.CurrentUsername)
 	if err != nil {
 		return u, err
 	}
