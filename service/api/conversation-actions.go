a0ee1f08bca3245dca6a97231b0757cec702b804
Flavia Bonanni <bonanni.1187711@studenti.uniroma1.it>
2025-05-21 12:42:59 +0200

    Update conversation-actions.go

--- service/api/conversation-actions.go
+++ service/api/conversation-actions.go
@@ -10,6 +10,32 @@ import (
 )
 
 
+func (rt *_router) getMyConversations(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
+	var user User
+	token := getToken(r.Header.Get("Authorization"))
+	user.ID = token
+	dbUser, err := rt.db.CheckUserById(user.ToDatabase())
+	if err != nil {
+    	http.Error(w, err.Error(), http.StatusInternalServerError)
+    	return
+	}
+	user.FromDatabase(dbUser)
+
+	username := ps.ByName("username")
+
+	// Get the user's conversations from the database
+	conversations, err := rt.db.GetConversations(username)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Respond with the conversations
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(conversations)
+}
+
 func (rt *_router) getConversation(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
     // 1) Estrai il token ed esegui l’autenticazione
     token := getToken(r.Header.Get("Authorization"))
@@ -39,32 +65,4 @@ func (rt *_router) getConversation(w http.ResponseWriter, r *http.Request, ps ht
     w.Header().Set("Content-Type", "application/json")
     w.WriteHeader(http.StatusOK)
     _ = json.NewEncoder(w).Encode(conv)
-}
-
-func (rt *_router) getConversation(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
-	var user User
-	var conversation database.Conversation
-	// estrarre un token dall'header
-	//token := getToken(r.Header.Get("Authorization"))
-	dbUser, err := rt.db.CheckUserById(user.ToDatabase())
-	if err != nil {
-    	http.Error(w, err.Error(), http.StatusInternalServerError)
-    	return
-	}
-	user.FromDatabase(dbUser)
-
-
-	conversationId := ps.ByName("conversation_id")
-
-	// Get the user's conversation from the database
-	conversation, err = rt.db.GetConversation(conversationId)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	// Respond with the conversation
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	_ = json.NewEncoder(w).Encode(conversation)
 }
