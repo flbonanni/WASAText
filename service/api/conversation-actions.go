40d145bfb9b2437ad7b99786efd59a583b21e7d5
Flavia Bonanni <bonanni.1187711@studenti.uniroma1.it>
2025-05-21 12:41:46 +0200

    Update conversation-actions.go

--- service/api/conversation-actions.go
+++ service/api/conversation-actions.go
@@ -10,30 +10,35 @@ import (
 )
 
 
-func (rt *_router) getMyConversations(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
-	var user User
-	token := getToken(r.Header.Get("Authorization"))
-	user.ID = token
-	dbUser, err := rt.db.CheckUserById(user.ToDatabase())
-	if err != nil {
-    	http.Error(w, err.Error(), http.StatusInternalServerError)
-    	return
-	}
-	user.FromDatabase(dbUser)
+func (rt *_router) getConversation(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
+    // 1) Estrai il token ed esegui l’autenticazione
+    token := getToken(r.Header.Get("Authorization"))
+    user := User{ID: token}
+    dbUser, err := rt.db.CheckUserById(user.ToDatabase())
+    if err != nil {
+        http.Error(w, "User does not exist", http.StatusUnauthorized)
+        return
+    }
+    user.FromDatabase(dbUser)
 
-	username := ps.ByName("username")
+    // 2) Prendi l’ID della conversazione dai path params
+    conversationID := ps.ByName("conversation_id")
 
-	// Get the user's conversations from the database
-	conversations, err := rt.db.GetConversations(username)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+    // 3) Recupera la conversazione
+    conv, err := rt.db.GetConversation(conversationID)
+    if err != nil {
+        if err == database.ErrConversationDoesNotExist {
+            http.Error(w, "Conversation does not exist", http.StatusNotFound)
+        } else {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+        return
+    }
 
-	// Respond with the conversations
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	_ = json.NewEncoder(w).Encode(conversations)
+    // 4) Rispondi con la conversazione
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    _ = json.NewEncoder(w).Encode(conv)
 }
 
 func (rt *_router) getConversation(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
